1b7542b2caf08b5f25024966bf3bf93e7cb76073
Mit Sudebov <sudebovds@gmail.com>
2022-12-27 14:50:46 +0300

    Fixed next config

--- next.config.js
+++ next.config.js
@@ -13,16 +13,6 @@ const nextConfig = {
             { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
             { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
           ]
-        },
-        {
-          // matching all API routes
-          source: "/*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ]
         }        
       ]
     }
