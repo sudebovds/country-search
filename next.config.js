4a213a0e43a9d9c36db29af3053e7282e2adf9b3
Mit Sudebov <66118089+sudebovds@users.noreply.github.com>
2022-12-27 15:07:26 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ const nextConfig = {
       return [
         {
           // matching all API routes
-          source: "/*/(*.*)",
+          source: "/**/(*.*)",
           headers: [
             { key: "Access-Control-Allow-Credentials", value: "true" },
             { key: "Access-Control-Allow-Origin", value: "*" },
